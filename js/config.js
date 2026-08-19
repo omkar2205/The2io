e59fdeb0e34a5432315e404a653c2394f1deeb3e
P Omkareshwar Reddy <1014573+omkar2205@users.noreply.github.com>
2026-08-20 00:10:36 +0530

    Add backend configuration module

---
+++ js/config.js
@@ -0,0 +1,6 @@
+window.THE2IO_CONFIG = Object.freeze({
+  // Paste the deployed Google Apps Script /exec URL here.
+  API_URL: "",
+  FALLBACK_CONTACT_URL: "https://formsubmit.co/the2ioprod@gmail.com",
+  JSONP_TIMEOUT_MS: 8000
+});
