5b0a403cf3ea906c8e7c01ba1d7178c5d90e9a1b
P Omkareshwar Reddy <1014573+omkar2205@users.noreply.github.com>
2026-08-20 00:47:19 +0530

    Connect The2iO frontend to Apps Script backend

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window.THE2IO_CONFIG = Object.freeze({
   // Paste the deployed Google Apps Script /exec URL here.
-  API_URL: "",
+  API_URL: "https://script.google.com/macros/s/AKfycbwysCJk_b7Yj88MooUKso5CRdxUnDhqjRdI1riga6sRMpm3TJSANQAf5NP2_NqQbwzaAQ/exec",
   FALLBACK_CONTACT_URL: "https://formsubmit.co/the2ioprod@gmail.com",
   JSONP_TIMEOUT_MS: 8000
 });
