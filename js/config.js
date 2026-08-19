f9c62c01675bbf0e1aa2fef576fd93b960990177
P Omkareshwar Reddy <1014573+omkar2205@users.noreply.github.com>
2026-08-20 00:52:39 +0530

    Use exact The2iO naming in frontend config

--- js/config.js
+++ js/config.js
@@ -1,4 +1,4 @@
-window.THE2IO_CONFIG = Object.freeze({
+window.The2iO_CONFIG = Object.freeze({
   // Paste the deployed Google Apps Script /exec URL here.
   API_URL: "https://script.google.com/macros/s/AKfycbwysCJk_b7Yj88MooUKso5CRdxUnDhqjRdI1riga6sRMpm3TJSANQAf5NP2_NqQbwzaAQ/exec",
   FALLBACK_CONTACT_URL: "https://formsubmit.co/the2ioprod@gmail.com",
